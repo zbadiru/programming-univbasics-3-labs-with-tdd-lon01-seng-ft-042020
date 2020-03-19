bd35ef6d7764a1d616ae798277a33f8bf8462751
Zakari Badiru <zbadiru@live.co.uk>
2020-03-19 12:49:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number = 30
 second_number = 5
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
